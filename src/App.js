ca79dc2d612942ed4f9d33eafadff7cdf50c943f
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2020-10-04 20:05:30 +0900

    recommend update

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import TodoList from './components/TodoList'
 import store from './store'       //store 의 index.js : todoReducer과 연결시키는 역할
 import {Provider} from 'react-redux'
 
+import Recommend from './components/Recommend'
+import './App.css';
+
 const App = () => {
 
   return <>
   {/* Provider는 위 폴더들을 연결해주는 역할 */}
   <Provider store = {store}>
     <div style={{width: "1000px",margin: "0 auto"}}>
-      <Signup/>
+      {/* <Signup/> */}
       {/* <TodoInput/>
       <TodoList/> */}
+      <Recommend/>
     </div>
   </Provider>
   </>
